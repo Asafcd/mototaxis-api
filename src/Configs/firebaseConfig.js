d0d38b32920152318a843508bb9b2de0714c4638
Asafcd <asafcontreras@gmail.com>
2023-04-17 10:35:08 -0600

    database changed

--- src/Configs/firebaseConfig.js
+++ src/Configs/firebaseConfig.js
@@ -1,5 +1,5 @@
 const { cert } = require('firebase-admin/app');
-const serviceAccount = require('../../project-fenix-7af65-firebase-adminsdk-uh3gi-d1facb18a9.json')
+const serviceAccount = require('../../moto-fenix-firebase-adminsdk-luuc8-220a2fd0d3.json')
 
 const firebaseConfig = { credential: cert(serviceAccount) }
 module.exports = firebaseConfig
