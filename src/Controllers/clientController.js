f0b1b305d22fda5942cf380f159a3b5ec372c2bc
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-09-18 13:53:30 -0600

    clientService add, delete, addtrip

--- src/Controllers/clientController.js
+++ src/Controllers/clientController.js
@@ -1,14 +1,18 @@
-const {getClientbyId} = require("../Services/clientService")
+const {getClientbyId, getTrips} = require("../Services/clientService")
 
 const getClient = async (req, res) => {
     const {id} = req.params
     try {
         const {status, data} = await getClientbyId(id)
-        if(status) { res.status(200).send({data}) }
+        if(status) { 
+            const { data: tripData} = await getTrips(data.historial)
+            const clientData = { ...data, historial: tripData}
+            res.status(200).send({clientData}) 
+        }
         else{
             res.status(400).send({data: `Client with id ${id} not found`})
         }
-        console.log(data)
+        
     } catch (err) {
         res
             .status(err?.status || 500)
--- src/Services/clientService.js
+++ src/Services/clientService.js
@@ -3,20 +3,120 @@ const firebase = require('../../firebase')
 const db = getFirestore(firebase)
 
 const clientCollection = db.collection('clientes')
+const tripsCollection = db.collection('viajes')
+const feeCollection = db.collection('pagos')
 
-const getClientbyId = async(id) => {
+const getClientbyId = async (id) => {
     try{
         const clientRef = clientCollection.doc(id)
         const clientDoc = await clientRef.get()
-
-        if(client.exists){ return {status:true, data: clientDoc.data()} }
-        else { return {status: false, data:"No such document"} }
+        
+        if(clientDoc.exists){ return {status:true, data: clientDoc.data()} }
+        else { return {status: false, data:"No such Client document"} }
         
     } catch (error) {
         throw { status: 500, error: error };
   }
 }
 
+ const getTrips = async (idRefs) => {
+    try {
+        const tripsRef = await tripsCollection.get()
+        const tripsDocs = tripsRef.docs
+        const trips = []
+        idRefs.forEach( (id) => {
+            const trip = tripsDocs.find( (doc) =>  doc.id == id)
+            const tripData = trip.data()
+            const feeId = trip.get("fee")._path.segments[1]
+            trips.push(tripData)
+            tripData.fee.get().then(documentSnapshot => {
+                if (documentSnapshot.exists) {
+                  console.log(documentSnapshot);
+                }
+        })
+        })
+        //trips.find((trip) => trip)
+        
+        return {status: true, data: trips}
+    } catch (error) {
+        throw { status: 500, error: error };
+    }
+ }
+
+ const getFee = async (idFee) => {
+    try {
+        const feeRef = feeCollection.doc(idFee)
+        const feeDoc = await feeRef.get()
+    
+        if(feeData.exists){ return {status:true, data: feeDoc.data()} }
+        else { return {status: false, data:"No such Fee document"} }
+        
+    } catch (error) {
+        throw { status: 500, error: error };
+    }
+ }
+
+ const addClient = async (client, profilePic) => {
+    try {
+        const clientRef = await clientCollection.add({...client,
+            historial: [],
+            rating: [5],
+            profilePic: profilePic,})
+        const clientDoc = await clientRef.get()
+        return {status: true, data: clientDoc.id()}
+    } catch (error) {
+        throw { status: 500, error: error };
+    }
+ }
+
+ const updateClient = async (client, id) => {
+    try{
+        const clientRef = clientCollection.doc(id)
+        const clientDoc = await clientRef.get()
+        
+        if(clientDoc.exists){ 
+            await clientRef.update(client)
+            return {status:true, data: clientDoc.data()} 
+        }
+        else { return {status: false, data:"No such Client document"} }
+        
+    } catch (error) {
+        throw { status: 500, error: error };
+    }
+ }
+
+ const addTripToClient = async (trip, rating) => {
+    try {
+        const clientRef = clientCollection.doc(trip.client)
+        const clientData = (await clientRef.get()).data()
+        clientData.historial.push(trip.id)
+        clientData.rating.push(rating)
+        await clientRef.update(clientData)
+        return {status: true, data: clientData}
+    } catch (error) {
+        throw { status: 500, error: error };
+    }
+ }
+
+ const deleteClient = async (id) => {
+    try{
+        const clientRef = clientCollection.doc(id)
+        if(clientRef.exists){
+            const clientDoc = await clientRef.delete()
+        } else {throw {status: false, data: "No such Client document"}}
+        return {status: true, data: clientDoc.data()}
+    } catch (error) {
+        throw { status: 500, error: error };
+    }
+}
+
+// TODO Chnage to picture, add the picture to the bucket
+
 module.exports = {
-    getClientbyId
+    getClientbyId,
+    getTrips,
+    addClient,
+    updateClient,
+    addTripToClient,
+    deleteClient
 }
