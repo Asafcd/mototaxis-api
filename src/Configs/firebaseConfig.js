2057b6f40e6a049c1ab2951551529f05403837c6
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-11-14 10:47:52 -0600

    removing firebase safely

--- src/Configs/firebaseConfig.js
+++
@@ -1,5 +0,0 @@
-const { cert } = require('firebase-admin/app');
-const serviceAccount = require('../../moto-fenix-firebase-adminsdk-luuc8-220a2fd0d3.json')
-
-const firebaseConfig = { credential: cert(serviceAccount) }
-module.exports = firebaseConfig
