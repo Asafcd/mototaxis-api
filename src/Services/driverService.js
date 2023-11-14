0ca9db6f595acb47247d9e19843f478f3cbda941
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-11-14 09:31:14 -0600

    driver with mongo, 90% .Lack few type validations

--- src/Services/driverService.js
+++ src/Services/driverService.js
@@ -1,6 +1,5 @@
 const { getFirestore } = require('firebase-admin/firestore');
 const firebase = require('../../firebase')
-const db = getFirestore(firebase)
 
 const driverSchema = require('../models/driver')
 
@@ -55,38 +54,10 @@ const deleteDriver = async (id) => {
 }
 
 
-const addTripToDriver = async (trip, rating) => {
-  try {
-    const driverRef = driverCollection.doc(trip.driverId)
-    const driverData = (await driverRef.get()).data()
-    driverData.historial.push(trip.travelId)
-    if (rating !== null) {
-      driverData.rating.push(rating)
-    }
-    await driverRef.update(driverData)
-    return { status: true, data: driverData }
-  } catch (error) {
-    throw { status: 500, error: error };
-  }
-}
-
-const driverOnline = async (id) => {
-  try {
-    const driverRef = driverCollection.doc(trip.driver)
-    const driverData = (await driverRef.get()).data()
-    driverData.disponibilidad = true
-    await driverRef.update(driverData)
-  } catch (error) {
-    throw { status: 500, error: error };
-  }
-}
-
 module.exports = {
   getDriver,
   getDrivers,
   createDriver,
   updateDriver,
   deleteDriver,
-  addTripToDriver,
-  driverOnline,
 };
