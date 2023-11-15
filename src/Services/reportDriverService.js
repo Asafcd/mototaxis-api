c49a758c0502e7831fb1dcb839833c2526665911
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-11-14 19:31:07 -0600

    reportServices implemented

---
+++ src/Services/reportDriverService.js
@@ -0,0 +1,46 @@
+const reportDriverSchema = require("../models/reportDriver");
+
+const createData = async (data) => {
+    try {
+        const reportDriver = new reportDriverSchema(data)
+        const newreportDriver = await reportDriver.save()
+        return {status: true, data: newreportDriver._id}
+    } catch (err) { throw { status: 500, error: err } }
+}
+
+const getData = async () => {
+    try {
+        return await reportDriverSchema.find()
+    } catch (err) { throw { status: 500, error: err } }
+}
+
+const getDataById = async (id) => {
+    try {
+        const dataRef = await reportDriverSchema.findById(id)
+        if(!dataRef) { return { status: false, data: "reportDriver does not exist" } }
+
+        return {status: true, data: dataRef}
+    } catch (err) { throw { status: 500, error: err } }
+}
+
+const updateData = async (id, data) => {
+    try{
+        const {status} = await getDataById(id)
+        if(!status) { return { status: false, data: "reportDriver does not exist" } }
+
+        const dataRef = await reportDriverSchema.findByIdAndUpdate(id, data)
+        return {status: true, data: dataRef}
+    }catch(err){throw {status: 500, error: err}}
+}
+
+const deleteData = async (id) => {
+    try{
+        const {status} = await getDataById(id)
+        if(!status) { return { status: false, data: "reportDriver does not exist" } }
+
+        const data = await reportDriverSchema.findByIdAndRemove(id)
+        return {status: true, data}
+    }catch(err){throw {status: 500, error: err}}
+}
+
+module.exports = { createData, getData, getDataById, updateData, deleteData }
---
+++ src/Services/reportUserService.js
@@ -0,0 +1,46 @@
+const reportUserSchema = require("../models/reportUser");
+
+const createData = async (data) => {
+    try {
+        const reportUser = new reportUserSchema(data)
+        const newreportUser = await reportUser.save()
+        return {status: true, data: newreportUser._id}
+    } catch (err) { throw { status: 500, error: err } }
+}
+
+const getData = async () => {
+    try {
+        return await reportUserSchema.find()
+    } catch (err) { throw { status: 500, error: err } }
+}
+
+const getDataById = async (id) => {
+    try {
+        const dataRef = await reportUserSchema.findById(id)
+        if(!dataRef) { return { status: false, data: "reportUser does not exist" } }
+
+        return {status: true, data: dataRef}
+    } catch (err) { throw { status: 500, error: err } }
+}
+
+const updateData = async (id, data) => {
+    try{
+        const {status} = await getDataById(id)
+        if(!status) { return { status: false, data: "reportUser does not exist" } }
+
+        const dataRef = await reportUserSchema.findByIdAndUpdate(id, data)
+        return {status: true, data: dataRef}
+    }catch(err){throw {status: 500, error: err}}
+}
+
+const deleteData = async (id) => {
+    try{
+        const {status} = await getDataById(id)
+        if(!status) { return { status: false, data: "reportUser does not exist" } }
+
+        const data = await reportUserSchema.findByIdAndRemove(id)
+        return {status: true, data}
+    }catch(err){throw {status: 500, error: err}}
+}
+
+module.exports = { createData, getData, getDataById, updateData, deleteData }
