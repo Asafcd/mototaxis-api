a3ac7475ce716d682c2845f9520ffd7bd0606e53
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-11-14 19:32:56 -0600

    reports controllers implemented

---
+++ src/Controllers/reportUserController.js
@@ -0,0 +1,54 @@
+const { createData, getData, getDataById, updateData, deleteData } = require("../Services/reportUserService")
+
+const create = async (req, res) => {
+  const body = req.body
+  try {
+    const {data} = await createData(body);
+    res.status(201).send({ data, message: "Report for User created succesfully" });
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err });
+  }
+}
+
+const get = async (req, res) => {
+  try {
+    const data = await getData();
+    res.status(200).send({ data });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+}
+
+const getById = async (req, res) => {
+  const {id} = req.params
+  try {
+    const data = await getDataById(id);
+    res.status(200).send({ data });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+}
+
+const update = async (req, res) => {
+  const {id} = req.params
+  const body = req.body
+  try {
+    const data = await updateData(id, body);
+    res.status(200).send({ data });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+}
+
+const deleteById = async (req, res) => {
+  const {id} = req.params
+  try {
+    const data = await deleteData(id);
+    res.status(200).send({ data });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+}
+
+module.exports = { create, get, getById, update, deleteById }
